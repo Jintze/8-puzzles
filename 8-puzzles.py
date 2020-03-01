088dce9ca73d7ccc1fa89fb9302d3ae48ac669f5
Jinzhe Li <32780791+Jintze@users.noreply.github.com>
2020-02-29 22:27:30 -0700

    Update 8-puzzles.py

--- 8-puzzles.py
+++ 8-puzzles.py
@@ -60,11 +60,12 @@ class State():
 
 ####################################################################################
 
-# def addActions(currentState):
-# 	newState.parent = currentState
-# 	newState.gncost += 1
-# 	newState.hncost = calculateManhattan(newState)
-# 	children.append(newState)
+def addActions(currentState, newState,children):
+	newState.parent = currentState
+	newState.gncost += 1
+	newState.hncost = calculateManhattan(newState)
+	newState.fncost = newState.gncost - newState.hncost
+	children.append(newState)
 
 def actions(currentState):
 	children = []
@@ -75,227 +76,136 @@ def actions(currentState):
 		newState = State(currentState.second, 0, currentState.third,
                             currentState.fourth, currentState.fifth, currentState.sixth,
                             currentState.seventh, currentState.eighth, currentState.nineth)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		# newState.parent = currentState
+		# newState.gncost += 1
+		# newState.hncost = calculateManhattan(newState)
+		# newState.fncost = newState.gncost - newState.hncost
+		# children.append(newState)
+		addActions(currentState, newState,children)
 		
 
 		newState = State(currentState.fourth, currentState.second, currentState.third,
                             0, currentState.fifth, currentState.sixth,
                             currentState.seventh, currentState.eighth, currentState.nineth)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		addActions(currentState, newState,children)
 
 	elif currentState.second == 0:
 		newState = State(0, currentState.first, currentState.third,
                             currentState.fourth, currentState.fifth, currentState.sixth,
                             currentState.seventh, currentState.eighth, currentState.nineth)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		addActions(currentState, newState,children)
 
 		newState = State(currentState.first, currentState.fifth, currentState.third,
                             currentState.fourth, 0, currentState.sixth,
                             currentState.seventh, currentState.eighth, currentState.nineth)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		addActions(currentState, newState,children)
 
 		newState = State(currentState.first, currentState.third, 0,
                             currentState.fourth, currentState.fifth, currentState.sixth,
                             currentState.seventh, currentState.eighth, currentState.nineth)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		addActions(currentState, newState,children)
     
 	elif currentState.third == 0:
 		newState = State(currentState.first, 0, currentState.second,
                             currentState.fourth, currentState.fifth, currentState.sixth,
                             currentState.seventh, currentState.eighth, currentState.nineth)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		addActions(currentState, newState,children)
 
 		newState = State(currentState.first, currentState.second, currentState.sixth,
                             currentState.fourth, currentState.fifth, 0,
                             currentState.seventh, currentState.eighth, currentState.nineth)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		addActions(currentState, newState,children)
     
 	elif currentState.fourth == 0:
 		newState = State(0, currentState.second, currentState.third,
                             currentState.first, currentState.fifth, currentState.sixth,
                             currentState.seventh, currentState.eighth, currentState.nineth)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		addActions(currentState, newState,children)
 
 		newState = State(currentState.first, currentState.second, currentState.third,
                             currentState.fifth, 0, currentState.sixth,
                             currentState.seventh, currentState.eighth, currentState.nineth)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		addActions(currentState, newState,children)
 
 		newState = State(currentState.first, currentState.second, currentState.third,
                             currentState.seventh, currentState.fifth, currentState.sixth,
                             0, currentState.eighth, currentState.nineth)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		addActions(currentState, newState,children)
     
 	elif currentState.fifth == 0:
 		newState = State(currentState.first, 0, currentState.third,
                             currentState.fourth, currentState.second, currentState.sixth,
                             currentState.seventh, currentState.eighth, currentState.nineth)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		addActions(currentState, newState,children)
 
 		newState = State(currentState.first, currentState.second, currentState.third,
                             0, currentState.fourth, currentState.sixth,
                             currentState.seventh, currentState.eighth, currentState.nineth)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		addActions(currentState, newState,children)
 
 		newState = State(currentState.first, currentState.second, currentState.third,
                             currentState.fourth, currentState.sixth, 0,
                             currentState.seventh, currentState.eighth, currentState.nineth)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		addActions(currentState, newState,children)
 
 		newState = State(currentState.first, currentState.second, currentState.third,
                             currentState.fourth, currentState.eighth, currentState.sixth,
                             currentState.seventh, 0, currentState.nineth)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		addActions(currentState, newState,children)
     
 	elif currentState.sixth == 0:
 		newState = State(currentState.first, currentState.second, 0,
                             currentState.fourth, currentState.fifth, currentState.third,
                             currentState.seventh, currentState.eighth, currentState.nineth)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		addActions(currentState, newState,children)
 
 		newState = State(currentState.first, currentState.second, currentState.third,
                             currentState.fourth, 0, currentState.fifth,
                             currentState.seventh, currentState.eighth, currentState.nineth)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		addActions(currentState, newState,children)
 
 		newState = State(currentState.first, currentState.second, currentState.third,
                             currentState.fourth, currentState.fifth, currentState.nineth,
                             currentState.seventh, currentState.eighth, 0)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		addActions(currentState, newState,children)
     
 	elif currentState.seventh == 0:
 		newState = State(currentState.first, currentState.second, currentState.third,
                             0, currentState.fifth, currentState.sixth,
                             currentState.fourth, currentState.eighth, currentState.nineth)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		addActions(currentState, newState,children)
 
 		newState = State(currentState.first, currentState.second, currentState.third,
                             currentState.fourth, currentState.fifth, currentState.sixth,
                             currentState.eighth, 0, currentState.nineth)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		addActions(currentState, newState,children)
     
 	elif currentState.eighth == 0:
 		newState = State(currentState.first, currentState.second, currentState.third,
                             currentState.fourth, currentState.fifth, currentState.sixth,
                             0, currentState.seventh, currentState.nineth)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		addActions(currentState, newState,children)
 
 		newState = State(currentState.first, currentState.second, currentState.third,
                             currentState.fourth, 0, currentState.sixth,
                             currentState.seventh, currentState.fifth, currentState.nineth)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		addActions(currentState, newState,children)
 
 		newState = State(currentState.first, currentState.second, currentState.third,
                             currentState.fourth, currentState.fifth, currentState.sixth,
                             currentState.seventh, currentState.nineth, 0)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		addActions(currentState, newState,children)
     
 	else:
 		newState = State(currentState.first, currentState.second, currentState.third,
                             currentState.fourth, currentState.fifth, 0,
                             currentState.seventh, currentState.eighth, currentState.sixth)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		addActions(currentState, newState,children)
 
 		newState = State(currentState.first, currentState.second, currentState.third,
                             currentState.fourth, currentState.fifth, currentState.sixth,
                             currentState.seventh, 0, currentState.eighth)
-		newState.parent = currentState
-		newState.gncost += 1
-		newState.hncost = calculateManhattan(newState)
-		newState.fncost = newState.gncost - newState.hncost
-		children.append(newState)
+		addActions(currentState, newState,children)
 
 	return children
 
@@ -303,19 +213,12 @@ def actions(currentState):
 
 
 def AStarSearch():
-	# userInput = list()
-	# for i in range(1, 4):
-	# 	for j in range(1, 4):
-	# 		print("input column " + str(i) + ", row " + str(j) + "'s number:")
-	# 		currInput = int(input())
-	# 		userInput.append(currInput)
-	# userInput = tuple(userInput)
-	# initialState = State(userInput[0],userInput[1],userInput[2],
-	# 					userInput[3],userInput[4],userInput[5],
-	# 					userInput[6],userInput[7],userInput[8])
-	# initialState = State(1,2,3,4,0,5,6,7,8)
-	initialState = State(2,1,0,3,4,5,6,7,8)
-	# initialState = State(2,3,6,1,5,0,4,7,8)
+	print("Enter 9 numbers (including 0): ")
+	one, two, three = input().split()
+	four, five, six = input().split()
+	seven, eight, nine = input().split() 
+	initialState = State(int(one), int(two), int(three), int(four), \
+				int(five), int(six), int(seven), int(eight), int(nine))
 	openList = list()
 	closeList = list()
 	openList.append(initialState)
@@ -334,25 +237,17 @@ def AStarSearch():
 				min = openList[i].fncost
 				currentMin = openList[i]
 		openList.remove(currentMin)
-		# if len(closeList) > 0:
 		closeList.insert(0,currentMin)
-		parent = currentMin.parent
-		if currentMin != initialState:
-			printNextInfo(moveCount, currentMin, parent)
-			printState(currentMin)
 		children = actions(currentMin)
 		moveCount +=1
 		for child in children:
 			if child not in closeList:
-				
 				if child.isGoal():
-					printNextInfo(moveCount, child, currentMin)
-					printState(child)
-					printMoveNum(moveCount)
 					return child
 				elif child not in openList:
 					openList.append(child)
 
+####################################################################################
 
 def printMoveNum(count):
 	print("===========================")
@@ -428,86 +323,43 @@ def printState(solution):
 			print(printList[i] + " " + printList[i+1] + " " + printList[i+2])
 		print("")
 
-# def printMove(lastState, nextState):
-# 	oldList = list()
-# 	oldList.append(str(lastState.first))
-# 	oldList.append(str(lastState.second))
-# 	oldList.append(str(lastState.third))
-# 	oldList.append(str(lastState.fourth))
-# 	oldList.append(str(lastState.fifth))
-# 	oldList.append(str(lastState.sixth))
-# 	oldList.append(str(lastState.seventh))
-# 	oldList.append(str(lastState.eighth))
-# 	oldList.append(str(lastState.nineth))
-
-# 	newList = list()
-# 	newList.append(str(nextState.first))
-# 	newList.append(str(nextState.second))
-# 	newList.append(str(nextState.third))
-# 	newList.append(str(nextState.fourth))
-# 	newList.append(str(nextState.fifth))
-# 	newList.append(str(nextState.sixth))
-# 	newList.append(str(nextState.seventh))
-# 	newList.append(str(nextState.eighth))
-# 	newList.append(str(nextState.nineth))
-
-# 	lastZeroIndex = 0
-# 	nextZeroIndex = 0
-# 	status = str
-
-# 	for i in range(0,9):
-# 		if oldList[i] == 0:
-# 			lastZeroIndex = i
-# 		if newList[i] == 0:
-# 			nextZeroIndex = i
-	
-# 	if ((nextZeroIndex//3) - (lastZeroIndex//3)) == 1:
-# 		status = "down"
-# 	elif ((nextZeroIndex//3) - (lastZeroIndex//3)) == -1:
-# 		status = "up"
-# 	elif ((nextZeroIndex%3) - (lastZeroIndex%3)) == 1:
-# 		status = "right"
-# 	else:
-# 		status = "left"
-
-	
-
-
-# def 
-# 		path = []
-# 		path.append(solution)
-# 		parent = solution.parent
-# 		while parent:
-# 			path.append(parent)
-# 			parent = parent.parent
-# 		print("It takes " + str(len(path)) + " moves")
-
-# def displayNext(solution):
-	
-	
+def printAll(solution):
+	path = []
+	path.append(solution)
+	parent = solution.parent
+	while parent:
+		path.insert(0, parent)
+		parent = parent.parent
+	for t in range(0,len(path)):
+		if t + 1 < len(path):
+			state = path[t]
+			nextState = path[t + 1]
+			printNextInfo(t + 1, nextState, state)
+			printState(nextState)
+	printMoveNum(len(path) - 1)
 
 ####################################################################################
 
 def calculateManhattan(currentState):
     manhattanDict = 0
     if currentState.first != 0:
-        manhattanDict -= abs(((currentState.first)%3) - 0) + abs((currentState.first-1)//3 - 0)
+        manhattanDict -= abs(((currentState.first)%3) - 0) + abs((currentState.first)//3 - 0)
     if currentState.second != 0:
-        manhattanDict -= abs(((currentState.second)%3) - 1) + abs((currentState.second-1)//3 - 0)
+        manhattanDict -= abs(((currentState.second)%3) - 1) + abs((currentState.second)//3 - 0)
     if currentState.third != 0:
-        manhattanDict -= abs(((currentState.third)%3) - 2) + abs((currentState.third-1)//3 - 0)
+        manhattanDict -= abs(((currentState.third)%3) - 2) + abs((currentState.third)//3 - 0)
     if currentState.fourth != 0:
-        manhattanDict -= abs(((currentState.fourth)%3) - 0) + abs((currentState.fourth-1)//3 - 1)
+        manhattanDict -= abs(((currentState.fourth)%3) - 0) + abs((currentState.fourth)//3 - 1)
     if currentState.fifth != 0:
-        manhattanDict -= abs(((currentState.fifth)%3) - 1) + abs((currentState.fifth-1)//3 - 1)
+        manhattanDict -= abs(((currentState.fifth)%3) - 1) + abs((currentState.fifth)//3 - 1)
     if currentState.sixth != 0:
-        manhattanDict -= abs(((currentState.sixth)%3) - 2) + abs((currentState.sixth-1)//3 - 1)
+        manhattanDict -= abs(((currentState.sixth)%3) - 2) + abs((currentState.sixth)//3 - 1)
     if currentState.seventh != 0:
-        manhattanDict -= abs(((currentState.seventh)%3) - 0) + abs((currentState.seventh-1)//3 - 2)
+        manhattanDict -= abs(((currentState.seventh)%3) - 0) + abs((currentState.seventh)//3 - 2)
     if currentState.eighth != 0:
-        manhattanDict -= abs(((currentState.eighth)%3) - 1) + abs((currentState.eighth-1)//3 - 2)
+        manhattanDict -= abs(((currentState.eighth)%3) - 1) + abs((currentState.eighth)//3 - 2)
     if currentState.nineth != 0:
-        manhattanDict -= abs(((currentState.nineth)%3) - 2) + abs((currentState.nineth-1)//3 - 2)
+        manhattanDict -= abs(((currentState.nineth)%3) - 2) + abs((currentState.nineth)//3 - 2)
     return manhattanDict
 
 ####################################################################################
@@ -515,7 +367,9 @@ def calculateManhattan(currentState):
 def main():
 	# Find the solution
     # SASearch()
-	AStarSearch()
+	
+	solution = AStarSearch()
+	printAll(solution)
 
 ####################################################################################
 
